b3b1cdd1fb90f2a1281ba23ed8f6c6d43f3d1799
Vasu Harshvardhan <42612183+iamvasuh@users.noreply.github.com>
2019-03-20 02:18:23 +0530

    Update ExampleInstrumentedTest.java

--- library/src/androidTest/java/com/user/vasu_x/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/user/vasu_x/ExampleInstrumentedTest.java
@@ -5,7 +5,6 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 /**
